8f1921ebad94241e38018da9336ae5a33a1fb149
Pablo-UMA <pol93ronda@hotmail.com>
2016-05-19 18:51:43 +0200

    primeros test

--- TDD/Array_Asociativo.java
+++ TDD/Array_Asociativo.java
@@ -1,4 +1,84 @@
 
-public class Array_Asociativo {
+public class Array_Asociativo extends Object {
 
+	public class Nodo{
+		public String clave;
+		public String valor;
+		public Nodo sig;
+		public Nodo(){
+		clave=null;
+		valor=null;
+		sig=null;
+		}
+	}
+	
+	Nodo act;
+	public Array_Asociativo(){
+		act=new Nodo();
+		act.sig=null;
+	}
+	
+	
+	public void put(String c, String v){
+		Nodo nuevo=new Nodo();
+		if(act.clave==null){
+			act.clave=c;
+			act.valor=v;
+		}else{
+			nuevo.clave=c;
+			nuevo.valor=v;
+			nuevo.sig=null;
+			act.sig=nuevo;
+		}
+		
+	}
+	
+	public String get(String clave){
+		Nodo aux=new Nodo();
+		aux=act;
+		while(aux.clave!=clave&&aux!=null){
+			aux=aux.sig;
+		}
+		return aux.valor;
+	}
+	
+	public String getOrElse(String clave, String defecto){
+		String res = defecto;
+		if(this.get(clave)!=null){
+			res=this.get(clave);
+		}
+		return res;
+	}
+	
+	public int size(){
+		Nodo aux = new Nodo();
+		int res=0;
+		while(aux!=null){
+			res++;
+		}
+		return res;
+	}
+	
+	public boolean remove(String clave){
+		Nodo current=new Nodo();
+		Nodo ant=new Nodo();
+		ant=act;
+		current=ant.sig;
+		boolean res=false;
+		if(ant.clave==clave){
+			res=true;
+			act.sig=null;
+		}else {
+			while(current.clave!=clave&&current.clave!=null){
+				ant=ant.sig;
+				current=current.sig;
+			}
+			if(current!=null){
+				current=current.sig;
+				res=true;
+				ant.sig=current;
+			}
+		}
+		return res;
+	}
 }
--- TDD/Test.java
+++ TDD/Test.java
@@ -13,8 +13,35 @@ public class Test {
 	}
 	
 	@org.junit.Test
-	public void addNewValueWithPutt() {
-		assertEquals(Array_Asociativo b = new Array_Asociativo("nombre","luis"), b.put("lus"));
+	public void putInAnEmptyArray()  {
+		
+	}
+	
+
+	@org.junit.Test
+	public void addNewValueWithPut() {
+		Array_Asociativo b = new Array_Asociativo();
+		b.put("nombre", "luis");
+		a.put("nombre", "luis");
+		assertEquals(a, b);
+	}
+	
+	@org.junit.Test
+	public void getValue()  {
+		a.put("nombre", "luis");
+		assertEquals("luis", a.get("nombre"));
+	}
+	
+	@org.junit.Test
+	public void getValueOrElseDefault()  {
+		a.put("nombre", "luis");
+		assertEquals("defecto",a.getOrElse("edad","defecto"));
+	}
+	
+	@org.junit.Test
+	public void getValueOrElse()  {
+		a.put("nombre", "luis");
+		assertEquals("luis", a.getOrElse("nombre", "defecto"));
 	}
 	
 }
